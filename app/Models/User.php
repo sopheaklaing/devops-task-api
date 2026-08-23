311cc15bbcd147829aa3aba0cbc1ce62cf316ffc
sol sopheak <sopheaksol71@gmail.com>
2026-08-23 16:00:27 +0700

    Revert "version : final version user"

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,11 @@
 
 namespace App\Models;
 
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
-
     protected $fillable = [
         'name',
         'email',
@@ -24,32 +18,14 @@ class User extends Authenticatable implements JWTSubject
         'remember_token',
     ];
 
-    protected function casts(): array
-
-    {
-        return [];
-    }
-
-    /**
-     * User has many tasks.
-     */
-    public function tasks(): HasMany
-    {
-        return $this->hasMany(Task::class, 'user_id');
-    }
-
-    /**
-     * JWT identifier.
-     */
+    // JWT identifier
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
-    /**
-     * JWT custom claims.
-     */
-    public function getJWTCustomClaims(): array
+    // JWT custom claims
+    public function getJWTCustomClaims()
     {
         return [];
     }
