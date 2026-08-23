2c6a6387e33c04544e2a0804c74e64e707dc831d
Sol Sopheak <sopheaksol71@gmail.com>
2026-08-23 16:19:14 +0700

    Reapply version final version user

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,10 +13,23 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return $request->is('api/*')
+                ? null
+                : route('login');
+        });
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
-    })->create();
+
+        $exceptions->shouldRenderJsonWhen(function (
+            Request $request,
+            Throwable $e
+        ) {
+            return $request->is('api/*')
+                || $request->expectsJson();
+        });
+
+    })
+    ->create();
