ef7f40d65a14225a94500953eee1b74ae1c13d9a
Sol Sopheak <sopheaksol71@gmail.com>
2026-08-26 14:41:45 +0700

    final step

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,7 +10,7 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable=['user_id', 'title','description'];
+    protected $fillable = ['user_id', 'title', 'description'];
 
     public function user(): BelongsTo
     {
